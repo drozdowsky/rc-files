edb5abd9d600fae9966f6753ed0c32a7f65cfdf6
drozdowsky <hdrozdow+github@pm.me>
2019-06-06 22:20:35 +0200

    RealShit cpb

--- .screenlayout/work.sh
+++ .screenlayout/work.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1-1 --off --output DP2 --mode 1680x1050 --pos 3840x30 --rotate normal
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2 --mode 1680x1050 --pos 3840x30 --rotate normal
