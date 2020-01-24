3a65684c374aa2540b071674834c26d6e726e81f
drozdowsky <hubert.drozdowski@mirumee.com>
2020-01-24 09:53:48 +0100

    macOS X interation

--- scripts/django_models.py
+++ scripts/django_models.py
@@ -2,36 +2,30 @@ import django.apps
 
 
 def related_info(field):
-    if field.related_model is None:
-        return ''
+    if getattr(field, "related_model", None) is None:
+        return ""
 
-    start = '===========>'
+    start = "===========>"
+    if field.__class__.__name__ == "ManyToOneRel":
+        start = "<==========="
 
-    if field.__class__.__name__ == 'ManyToOneRel':
-        start = '<==========='
-
-    return '{}\t{}'.format(start, field.related_model)
+    return "{}\t{}".format(start, field.related_model)
 
 
 def main():
-    with open('django_models.txt', 'w') as f:
-
+    with open("django_models.txt", "w") as f:
         for model in django.apps.apps.get_models(include_auto_created=True):
             f.write("'{}'".format(model.__name__))
-            f.write('\n')
+            f.write("\n")
             f.write(str(model))
-            f.write('\n')
+            f.write("\n")
 
             for field in model._meta.get_fields():
-
-                f.write('\t')
+                f.write("\t")
                 f.write(field.name.ljust(40))
                 f.write(field.__class__.__name__.ljust(25))
                 f.write(related_info(field))
-                f.write('\n')
+                f.write("\n")
 
-            f.write('\n'.rjust(155, '='))
-            f.write('\n')
-
-
-main()
+            f.write("\n".rjust(155, "="))
+            f.write("\n")
